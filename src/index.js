97a74e850fd4752e1dc987802cb5c17642a7c4ee
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-08-28 18:56:29 +0200

    Refactored side-panel
    -Moved everything to separate js file
    
    -Added functionality to add new projects.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import { appendToBody, createDomElement, appendToTargetClass, appendChildrenToPa
 import { createElement } from "./element/element";
 import { toggleForm } from "./form/form";
 import { createTodoItem } from "./todo_item/todo_item";
+import { initSidePanel } from "./side-panel/side-panel";
 import './style.css'
 
 
 //Create main page elements
 const header = createElement('header', null, 'div', null);
 const headerTitle = createElement('title', null, 'h1', 'TODO Orginazer')
-const sidePanel = createElement('side-panel', null, 'div', null);
+//const sidePanel = createElement('side-panel', null, 'div', null);
 const mainContent = createElement('main-content', null, 'div', null);
 
 //Create an array of DOM elements for header, sidepanel and main content
-const mainPageElementsDOM = createDomElement(header, sidePanel, mainContent);
+//const mainPageElementsDOM = createDomElement(header, sidePanel, mainContent);
+const mainPageElementsDOM = createDomElement(header, mainContent);
+initSidePanel();
+
 //Add to document.body
 appendToBody(mainPageElementsDOM);
 
@@ -22,16 +26,7 @@ const headerTitleDOM = createDomElement(headerTitle);
 //Append title to header
 appendToTargetClass('.header', headerTitleDOM);
 
-//Create side panel items
-const topItemContainer = createElement('top-ul', null, 'ul', null);
-const allTodosItem = createElement('side-panel-item', 'allTodos', 'li', "All tasks");
-const urgentTodosItem = createElement('side-panel-item', 'urgentTodos', 'li', "Urgent tasks");
-const addProjectButton = createElement('add-button', 'addProjectButton', 'p', 'Add Project +')
-
-const topItemContainerDOM = createDomElement(topItemContainer);
-const topItemContainerChildren = createDomElement(allTodosItem, urgentTodosItem, addProjectButton);
-
-//---MAIN CONTENT---
+//### MAIN CONTENT
 //Add task button
 const addTaskButton = document.createElement('button');
 addTaskButton.setAttribute('id', 'addTaskButton');
@@ -39,6 +34,3 @@ addTaskButton.innerText = '+';
 appendToTargetClass('.main-content', [addTaskButton]);
 addTaskButton.addEventListener('click', toggleForm);
 
-//Add side panel items to the DOM as children
-appendToTargetClass('.side-panel', topItemContainerDOM);
-appendToTargetClass('.top-ul', topItemContainerChildren);
---
+++ src/side-panel/side-panel.js
@@ -0,0 +1,68 @@
+//Doc fragment for creating the DOM tree
+let fragmnt = document.createDocumentFragment();
+
+export const PROJECTS = [];
+//Items array to store the different menu options.
+const items = ['All tasks',
+    'Urgent tasks'
+];
+
+
+//Creating the main Side panel div
+const sidePanel = document.createElement('div');
+sidePanel.classList.add('side-panel');
+
+//Creating the top list which contains the Side panels
+//diffrent menu options
+const topList = createUl(items);
+const projectList = createUl(PROJECTS);
+topList.innerHTML += '<li id="addProjectButton">Add project</li>';
+topList.innerHTML += '<li><input id="submit_on_enter" class="hidden" type="text" name="q" placeholder="Enter project name"></li>';
+projectList.innerHTML += '<li><h3>Projects</h3></li>';
+sidePanel.append(topList, projectList);
+
+//Add eventlistner to add project button to create an input field when clicked on.
+const addProjectBtn = sidePanel.querySelector('#addProjectButton');
+const inputField = sidePanel.querySelector('#submit_on_enter');
+addProjectBtn.addEventListener('click', () => {
+    inputField.classList.remove('hidden');
+});
+
+//Listen to the enter key pressed when adding a new project and create the new project.
+inputField.addEventListener('keydown', (e) => {
+    if (e.key == 'Enter') {
+        addToProjectArray(inputField.value);
+        inputField.classList.add('hidden');
+        appendProjectToDom();
+        resetProjectInput();
+    };
+});
+//Store the entire Side panel inside of the fragment
+fragmnt.appendChild(sidePanel);
+
+//Function to aid in creating UL's, takes an array of items.
+function createUl(items) {
+    let ul = document.createElement('ul');
+    for (let i = 0; i < items.length; i++) {
+        ul.innerHTML += `<li>${items[i]}</li>`;
+    }
+    return ul;
+};
+
+function addToProjectArray(name) {
+    PROJECTS.push(`<li>${name}</li>`);
+};
+
+function appendProjectToDom() {
+    return sidePanel.lastChild.innerHTML += PROJECTS[PROJECTS.length - 1];
+}
+
+function resetProjectInput() {
+    inputField.value = "";
+};
+
+
+//Function to be used in index.js to initialize the side panel
+export function initSidePanel() {
+    return document.body.appendChild(fragmnt);
+}
